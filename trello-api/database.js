cfe1cb94c72f0e853fc27b4e4aa3646addef7815
Mr.Makako <diegofdiaz1717@gmail.com>
2023-05-10 23:04:29 -0600

    creud de cards y boards en construccion, faltan validadciones para cards, create y get funcionan

--- trello-api/database.js
+++ trello-api/database.js
@@ -1,24 +1,19 @@
+const mysql2 = require("mysql2");
+
+const pool = mysql2
+  .createPool({
+    host: "localhost",
+    user: "root",
+    password: "200400672",
+    database: "trellodb",
+    port: "3306",
+
+    connectionLimit: 10, // 连接池数
+    queueLimit: 4, // 排队限制
+  })
+  .promise();
+
+module.exports = pool;
 //createConnection , tenemos que  abrir y cerrar conecciones.
 //pool permite multiple conneccion  sin necesidad de abrir cerrar ya que lo manejara por nosotros.
 //en el pool en vez de cerrar la conexion se espera a que otro susuario se conecte a ella mejorando el rendimiento.
-
-const mysql2 = require("mysql2");
-
-const pool = mysql2.createPool({
-  host: "localhost",
-  user: "root",
-  password: "200400672",
-  database: "trellodb",
-  port: "3306",
-
-  connectionLimit: 10, // 连接池数
-  queueLimit: 4, // 排队限制
-});
-
-const promisePool = pool.promise();
-pool.getConnection((err, connection) => {
-  if (err) throw err;
-  console.log("Database connected successfully");
-  connection.release();
-});
-module.exports = pool;
