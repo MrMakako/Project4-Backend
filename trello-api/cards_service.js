a40f30022d8e57e95c87c3d0591fbd6620e10881
Mr.Makako <diegofdiaz1717@gmail.com>
2023-05-11 20:30:51 -0600

    ready

--- trello-api/cards_service.js
+++ trello-api/cards_service.js
@@ -3,17 +3,30 @@ const pool = require("./database");
 async function delete_Card() {}
 
 async function add_card(name, desc, list_id, position) {
+  console.log("card_inserted");
   await pool.query("Call createCard(?,?,?,?)", [name, desc, list_id, position]);
 }
 
-async function update_card() {}
+async function delete_All_Cards() {
+  await pool.query("call deleteAllcards()");
+}
+async function delete_Card(card_id) {
+  await pool.query("call deleteCard(?)", [card_id]);
+}
 
-async function getCards(list_id) {
-  const result = await pool.query("Call getCards(?)", [list_id]);
+async function update_card(card_id, name, description) {
+  await pool.query("Call updateCard(?,?,?)", [card_id, name, description]);
+}
+
+async function getCards(board_id) {
+  const result = await pool.query("Call getCards(?)", [board_id]);
   return result;
 }
 
 module.exports = {
   getCards,
   add_card,
+  update_card,
+  delete_All_Cards,
+  delete_Card,
 };
