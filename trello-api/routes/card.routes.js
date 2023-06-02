90df00c221ad6c33be3d2777617faba7bc0f7f5c
dessire <dessireochoa12@gmail.com>
2023-06-02 16:26:13 -0600

    Auth

--- trello-api/routes/card.routes.js
+++ trello-api/routes/card.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const cardController = require("../controller/card.controller");
-//const { authenticateToken } = require("../auth/middleware");
+const { authenticateToken } = require("../auth/middleware");
 
 //router.get("/all", authenticateToken, cardController.getCards);
 router.get("/all", cardController.getCards);
