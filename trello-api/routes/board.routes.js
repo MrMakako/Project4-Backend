f521ffbb06e2f5703e63d6b48f3fff67c006554c
dessire <dessireochoa12@gmail.com>
2023-06-02 22:00:49 -0600

    Auth

--- trello-api/routes/board.routes.js
+++ trello-api/routes/board.routes.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const boardController = require("../controller/board.controller");
-//const { authenticateToken } = require("../auth/middleware");
+const { authenticateToken } = require("../auth/middleware");
 
-//router.get("/all", authenticateToken, boardController.getCards);
-router.post("/all", boardController.getBoards);
+router.post("/all", authenticateToken, boardController.getBoards);
 router.post("/add", boardController.createBoard);
 module.exports = router;
--- trello-api/server.js
+++ trello-api/server.js
@@ -1,11 +1,12 @@
-const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = 3006;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const cors = require("cors");
+
 app.use(cors());
-//
+
 const cardRouter = require("./routes/card.routes");
 const boardRouter = require("./routes/board.routes");
 const userRouter = require("./routes/user.routes");
